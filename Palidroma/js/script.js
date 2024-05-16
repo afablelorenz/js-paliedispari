d80006310618ef1564ae33d574c21d71c8ce10d1
afablelorenz <afablelorenz@gmail.com>
2024-05-16 18:43:59 +0200

    Palindroma - finished reverseString function

--- Palidroma/js/script.js
+++ Palidroma/js/script.js
@@ -1,9 +1,18 @@
 let utenteString = prompt('Inserisci una stringa');
 
+reverseString(utenteString);
+
 function reverseString(string){
     let reversed = '';
 
-    for(let i = 0; i < string.length; i++){
-        
+    for(let i = string.length - 1; i >= 0; i--){
+        reversed += string[i];
+    }
+    if (string === reversed){
+        console.log(string,reversed);
+        console.log('E\' una palindroma!');
+    }else{
+        console.log(string,reversed);
+        console.log('Non è una palindroma');
     }
 }
