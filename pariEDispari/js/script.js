81f11ecb79f647b1db387c1bba6d1308b96f86b9
afablelorenz <afablelorenz@gmail.com>
2024-05-21 02:23:46 +0200

    pariEDispari - created functions

--- pariEDispari/js/script.js
+++ pariEDispari/js/script.js
@@ -1,3 +1,4 @@
+const numComputer = generateRandom();
 let pariDispariUser = prompt('Pari o Dispari').trim().toLowerCase();
 while (pariDispariUser !== 'pari' && pariDispariUser !== 'dispari'){
     alert('Inserire pari o dispari!');
@@ -7,5 +8,31 @@ while (pariDispariUser !== 'pari' && pariDispariUser !== 'dispari'){
 let numUtente = Number.parseInt(prompt('scegli un numero da 1 a 5').trim(),10);
 while (Number.isNaN(numUtente) || numUtente < 1 || numUtente > 5){
     alert('Inserire un numero da 1 a 5!');
-    Number.parseInt(prompt('scegli un numero da 1 a 5').trim(),10);
+    numUtente = Number.parseInt(prompt('scegli un numero da 1 a 5').trim(),10);
 }
+
+
+
+console.log('Hai scelto '+ pariDispariUser + ' , ' + numUtente);
+let somma = numComputer + numUtente;
+console.log('Il computer ha scelto ' + numComputer);
+
+if (pariDispari(somma)){
+    console.log('Hai vinto');
+}else{
+    console.log('Hai perso');
+}
+
+function generateRandom(){
+    const numRandom = Math.floor(Math.random() * 5) + 1;
+    return numRandom;
+}
+
+function pariDispari(somma){
+    if (somma % 2 === 0){
+        return true;
+    }else{
+        return false;
+    }
+}
+
